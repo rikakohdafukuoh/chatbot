f695669f60126ee79cdf3e03f3ed27688cc6779a
神田 理樺 <76685329+rikakohdafukuoh@users.noreply.github.com>
2024-02-23 11:14:56 +0000

    あ

--- app.py
+++ app.py
@@ -182,7 +182,7 @@ if st.session_state["messages"]:
 
     for message in reversed(messages[1:]):  # 直近のメッセージを上に
         speaker = "🙂"
-        if message["role"]=="assistant":
+        if message["role"]=="system":
             speaker="🤖"
 
         st.write(speaker + ": " + message["content"])
